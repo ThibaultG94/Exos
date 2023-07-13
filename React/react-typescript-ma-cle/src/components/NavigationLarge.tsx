1f199c62280284fcba5b4d41b2d25e6f4dc724fd
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-13 17:21:29 +0200

    react-typescript-ma-cle

---
+++ React/react-typescript-ma-cle/src/components/NavigationLarge.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const NavigationLarge = () => {
+	return (
+		<nav>
+			<ul className="header-navbar">
+				<li className="navbar_item">
+					<a href="/">Ma Clé</a>
+					<div className="trick"></div>
+					<ul className="navbar_under-item">
+						<li>Ma Clé Expertise</li>
+						<li>La Location Meublée</li>
+					</ul>
+				</li>
+				<li className="navbar_item">
+					<a href="/">Conciergerie Locative</a>
+				</li>
+				<li className="navbar_item">
+					<a href="/">Bail Mobilité</a>
+				</li>
+				<li className="navbar_item">
+					<a href="/">Nos tarifs</a>
+				</li>
+				<li className="navbar_item">
+					<a href="/">Proposez votre bien</a>
+				</li>
+				<li className="navbar_item">
+					<a href="/">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavigationLarge;
---
+++ React/react-typescript-ma-cle/src/components/NavigationMiddle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavigationMiddle = () => {
+	return (
+		<nav className="nav-responsive">
+			<button className="toggle-menu">
+				<span className="toggle-menu-content">
+					<span className="toggle-menu-lines">
+						<span className="toggle-menu-line"></span>
+						<span className="toggle-menu-line"></span>
+						<span className="toggle-menu-line"></span>
+					</span>
+					<span className="toggle-menu-label">Menu</span>
+				</span>
+			</button>
+		</nav>
+	);
+};
+
+export default NavigationMiddle;
