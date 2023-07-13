31583387ac6a1040fae352645aad30a4b99e8441
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-13 15:46:23 +0200

    react-firebase-toolkit

---
+++ React/react-firebase-toolkit/src/components/Delete.js
@@ -0,0 +1,23 @@
+import { deleteDoc, doc } from 'firebase/firestore';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../feature/post.slice';
+import { db } from '../utils/firebase.config';
+
+const Delete = ({ postId }) => {
+	const dispatch = useDispatch();
+
+	const handleDelete = () => {
+		deleteDoc(doc(db, 'posts', postId)).then(() => {
+			dispatch(deletePost(postId));
+		});
+	};
+
+	return (
+		<span className="delete" onClick={(e) => handleDelete(e)}>
+			<i className="fa-solid fa-trash-can"></i>
+		</span>
+	);
+};
+
+export default Delete;
